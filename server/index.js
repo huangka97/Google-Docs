1169c0378fda8408930e23c0835eb532d2e75aaa
Karl Huang <karl.huang2009@gmail.com>
2018-08-06 11:05:42 -0700

    set up completed

--- server/index.js
+++ server/index.js
@@ -1,4 +1,27 @@
 import http from 'http';
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+// set passport middleware to first try local strategy
+passport.use(new LocalStrategy(function(user, pass, cb){...});
+
+// session configuration
+passport.serializeUser(...);
+passport.deserializeUser(...);
+
+// connect passport to express via express middleware
+app.use(passport.initialize());
+app.use(passport.session());
+server.listen(8080);
+io.on('connection', function (socket) {
+  socket.emit('msg', { hello: 'world' });
+  socket.on('cmd', function (data) {
+    console.log(data);
+  });
+});
 
 http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,38 @@
 import React from 'react';
-
+import RaisedButton from 'material-ui/RaisedButton';
+import {Editor, EditorState, RichUtils} from 'draft-js';
+<RaisedButton color="primary">Bold</RaisedButton>
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      editorState: EditorState.createEmpty()
+    };
+    this.onChange = (editorState) => this.setState({editorState});
+  }
+
+  _onBoldClick(e) {
+    e.preventDefault()
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
+  }
+  const socket = io('http://localhost:8080');
+  componentDidMount() {
+    const socket = io('http://localhost:8080');
+    socket.on('connect', function() {
+      console.log('ws connect')
+    });
+    socket.on('disconnect', function() {
+      console.log('ws disconnect')
+    });
+    socket.on('msg', function(data) {
+      console.log('ws msg:', data);
+      socket.emit('cmd', {foo: 123})
+    });
+  }
   render() {
     return (<div>
-      <h2>Welcome to React!</h2>
+      <button onMouseDown={(e) => this._onBoldClick(e)}>BOLD</button>
+      <Editor editorState={this.state.editorState} onChange={this.onChange}/>
     </div>);
   }
 }
