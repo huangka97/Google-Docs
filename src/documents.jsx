00a19a8cf7335f8ff087394d88a09dafe8a41dff
Kitan Garcia <kitangar@gmail.com>
2018-08-08 10:00:52 -0700

    add password input and fetch in documentPortal

--- src/documents.jsx
+++ src/documents.jsx
@@ -5,7 +5,8 @@ export default class Documents extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      docToCreate: "",
+      docToCreateTitle: "",
+      docToCreatePassword: "",
       documents: [],
       docID: ""
     };
@@ -13,15 +14,47 @@ export default class Documents extends React.Component {
 
   handleDocTitle(event)//get input for doc title
   {
-    this.setState({docsToCreate: event.target.value});
+    this.setState({docToCreateTitle: event.target.value});
+  }
+
+  handleDocPassword(event)//get input for doc title
+  {
+    this.setState({docToCreatePassword: event.target.value});
   }
 
   createDoc(event)//create document when button is pressed
   {
     event.preventDefault();//finish this...
-//    fetch("")
-  this.props.documentFunction(event);
-  }
+    fetch("http://localhost:8080/create", {
+      method: "POST",
+      credentials: "same-origin",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({
+        title: this.state.docToCreateTitle, 
+        password: this.state.docToCreatePassword,
+        contents: "",
+        url: "",
+        ownerOfDoc: "",
+        collabsOfDoc: ""
+      })
+    })
+    .then((res) => {
+      console.log("DOCUMENT RES IS: ", res);
+       return res.json();
+     })
+     .then((json) => {
+       console.log("DOCUMENT JSON IS ": json);
+       if (json.success === true)
+       {
+         this.setState({
+           docToCreateTitle: "",
+           docToCreatePasssword: "",
+         });
+        //INSERT TOGGLE TO GET TO EDITOR
+       }
+     })
+     .catch((error) => console.log("Error: ", error));
+   }
 
   handleDocID(event)
   {
@@ -35,6 +68,7 @@ export default class Documents extends React.Component {
   }
 
 //LINKS OF DOCUMENTS SHOULD BE CLICKABLE AND REDIRECT CORRECTLY
+///create is the route
 
 
   render() {
@@ -42,6 +76,7 @@ export default class Documents extends React.Component {
       <div>
         <h1>Documents Portal</h1>
         <input type = "text" placeholder = "New document title" onChange = {(event) => this.handleDocTitle}/>
+        <input type = "password" placeholder = "New document password" onChange = {(event) => this.handleDocPassword}/>
         <button onClick = {(event) => this.createDoc(event)}>Create Document</button>
         {/* <ul>{this.state.documents.map((doc) => <li>{doc}</li>}</ul> */}
         <input type = "text" placeholder = "paste a doc ID shared with you" onChange = {(event) => this.handleDocID}/>
